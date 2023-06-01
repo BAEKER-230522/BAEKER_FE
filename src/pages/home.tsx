e15181dff4a4230544bc965e38c0ddbf421826e4
jeongdopark <jdpark0816@gmail.com>
2023-06-01 18:50:08 +0900

    style: home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,20 @@
+import { S } from "./home.styled";
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <div>
+          알고리즘 실력 향상은 BAEKER!
+          <br />
+          알고리즘 공부를 위한 스터디 그룹을 만들고 가입하세요.
+          <br />
+          다양한 규칙 수행 보상을 통해 스터디 랭커에 도전해 보세요!
+        </div>
+        <S.KakaoButton />
+      </S.Wrapper>
+    </S.Container>
+  );
+};
+
+export default Home;
