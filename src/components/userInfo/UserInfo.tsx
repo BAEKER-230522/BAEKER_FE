fbe88200c81a4b681d8d5d4b2785b541813487b6
jeongdopark <jdpark0816@gmail.com>
2023-06-27 19:26:12 +0900

    feat: 유저 정보 가져오기 api

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -1,13 +1,17 @@
 import { IMG_URL } from "@/pages/mock";
 import { S } from "./styled";
 import Link from "next/link";
+import { memberApi } from "@/api/memberApi";
 
 const UserInfo = () => {
+  const { data: memberData, error, isLoading } = memberApi.useGetMemberQuery(1);
+
+  if (isLoading) return <div>Loading...</div>;
   return (
     <S.Container>
       <S.Image src={IMG_URL} />
-      <S.Name>박정도</S.Name>
-      <S.Introduce>테스트입니다</S.Introduce>
+      <S.Name>{memberData.data.username}</S.Name>
+      <S.Introduce>{memberData.data.about}</S.Introduce>
       <Link href={"/modify"}>
         <S.Button>프로필 수정</S.Button>
       </Link>
