007a303bfce03c9bfeb232e984180825c0d9b7ce
jeongdopark <jdpark0816@gmail.com>
2023-06-29 16:45:14 +0900

    feat: 유저 정보 업데이트

--- src/pages/modify.styled.ts
+++ src/pages/modify.styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.form`
   width: 100vw;
   height: 95vh;
   display: flex;
--- src/pages/modify.tsx
+++ src/pages/modify.tsx
@@ -2,12 +2,41 @@ import { S } from "./modify.styled";
 import ModifyImg from "@/components/modify/Img";
 import Input from "@/components/common/Input";
 import ModifyButton from "@/components/modify/button";
+import { memberApi } from "@/api/memberApi";
+import { useState, useEffect } from "react";
+import { Router, useRouter } from "next/router";
+
+export interface IUserInfo {
+  nickname: string;
+  about : string;
+}
+
 const Modify = () => {
+  const {data, isLoading} = memberApi.useGetMemberQuery(1);
+  const [updateUserInfo, {isLoading: isUpdating}] = memberApi.useUpdateMemberMutation();
+  const [userInfo, setUserInfo] = useState<IUserInfo>({nickname:'', about: ''})
+  const router = useRouter();
+  
+  
+  useEffect(() => {
+    console.log(data, '1');
+    if(isLoading === false){
+      console.log(data, '2');
+      setUserInfo({nickname:data.data.nickname, about: data.data.about})
+    } 
+  }, [isLoading])
+
+  
+  if(isLoading) return <div>Loading...</div>
   return (
-    <S.Container>
+    <S.Container onSubmit={(e) => {
+      e.preventDefault();
+      updateUserInfo({"id":1, "nickname":userInfo.nickname, "about":userInfo.about})
+      router.push({pathname:"/profile"})
+    }}>
       <ModifyImg />
-      <Input title={"이름"} size={"25%"} />
-      <Input title={"자기소개"} size={"25%"} />
+      <Input title={"이름"} size={"25%"} value={userInfo.nickname} userInfo={userInfo} setUserInfo={setUserInfo}/>
+      <Input title={"자기소개"} size={"25%"} value={userInfo.about} userInfo={userInfo} setUserInfo={setUserInfo}/>
       <ModifyButton />
     </S.Container>
   );
