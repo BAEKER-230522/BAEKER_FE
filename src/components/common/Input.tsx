5313d0cfdbb5026b12e6c98b11d9a1c308d22aff
jeongdopark <jdpark0816@gmail.com>
2023-06-29 16:48:57 +0900

    feat: 유저 정보 업데이트

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,15 +1,27 @@
+import React from "react";
 import { S } from "./styled";
+import { IUserInfo } from "@/pages/modify";
 
 interface IInput {
   title: string;
   size: string;
+  value : string;
+  userInfo : IUserInfo;
+  setUserInfo: React.Dispatch<React.SetStateAction<IUserInfo>>;
 }
 
-const Input = ({ title, size }: IInput) => {
+const Input = ({ title, size, value, setUserInfo, userInfo }: IInput) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(title === "이름"){
+      setUserInfo({...userInfo, nickname:e.target.value})
+    }else{
+      setUserInfo({...userInfo, about: e.target.value})
+    }
+  }
   return (
     <S.InputContainer size={size}>
       <S.Title>{title}</S.Title>
-      <S.Input />
+      <S.Input onChange={onChange} value={value} />
     </S.InputContainer>
   );
 };
--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 import { memberApi } from "@/api/memberApi";
 
 const UserInfo = () => {
-  const { data: memberData, error, isLoading } = memberApi.useGetMemberQuery(1);
-
+  const { data, error, isLoading } = memberApi.useGetMemberQuery(1);
+  
   if (isLoading) return <div>Loading...</div>;
   return (
     <S.Container>
       <S.Image src={IMG_URL} />
-      <S.Name>{memberData.data.username}</S.Name>
-      <S.Introduce>{memberData.data.about}</S.Introduce>
+      <S.Name>{data.data.nickname}</S.Name>
+      <S.Introduce>{data.data.about}</S.Introduce>
       <Link href={"/modify"}>
         <S.Button>프로필 수정</S.Button>
       </Link>
