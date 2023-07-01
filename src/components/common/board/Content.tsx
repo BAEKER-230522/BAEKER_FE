db0e2b4b6138f99e6069bea471a36a34eee49e55
jeongdopark <jdpark0816@gmail.com>
2023-07-01 23:34:27 +0900

    feat: 게시판 목록

--- src/components/common/board/Content.tsx
+++ src/components/common/board/Content.tsx
@@ -1,162 +1,30 @@
 import { S } from "./styled";
 import { IBoard } from "./Board";
+import { useRouter } from "next/router";
 
 // get api로 데이터 받기.
-const mock = [
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "12",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "3/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "4/5",
-    host: "방장",
-    status: "123",
-  },
-  {
-    title: "32",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "4521",
-  },
-  {
-    title: "2",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "5123",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "study",
-    intro: " ipsum dolor sit amet consectetur",
-    members: "3/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "4/5",
-    host: "방장",
-    status: "123",
-  },
-  {
-    title: "4",
-    intro: " sit amet consectetur",
-    members: "1/5",
-    host: "방장",
-    status: "4521",
-  },
-  {
-    title: "5",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "51231234",
-  },
-  {
-    title: "6",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "3/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "3",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "4/5",
-    host: "방장",
-    status: "123",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "4521",
-  },
-  {
-    title: "12",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "5123",
-  },
-  {
-    title: "32",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "13",
-    intro: " ipsum dolor sit amet consectetur",
-    members: "3/5",
-    host: "방장",
-    status: "상태",
-  },
-  {
-    title: "6",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "4/5",
-    host: "방장",
-    status: "123",
-  },
-  {
-    title: "study",
-    intro: " sit amet consectetur",
-    members: "1/5",
-    host: "방장",
-    status: "4521",
-  },
-  {
-    title: "study",
-    intro: "Lorem, ipsum dolor sit amet consectetur",
-    members: "2/5",
-    host: "방장",
-    status: "51231234",
-  },
-];
 
-const Content = ({ target_nth, ratio, crntPage }: IBoard) => {
-  const test = mock.slice(crntPage! * 5, crntPage! * 5 + 5);
+interface IContentProps{
+  target_nth?: number;
+  ratio?: number;
+  crntPage?: number; 
+  data: any;
+  type:string;
+  category: string[];
+}
 
+const Content = ({ target_nth, ratio, crntPage, data, type, category }: IContentProps) => {
+  
+  const test = data.slice(crntPage! * 5, crntPage! * 5 + 5);
+  const router = useRouter()
+  
   return (
-    <S.ContentContainer>
-      {test.map((e, idx) => (
-        <S.ContentWrapper key={idx} target_nth={target_nth!} ratio={ratio!}>
-          <div>{e.title}</div>
-          <div>{e.intro}</div>
-          <div>{e.members}</div>
-          <div>{e.host}</div>
-          <div>{e.status}</div>
+    <S.ContentContainer >
+      {test.map((e:any, idx:number) => (
+        <S.ContentWrapper key={idx} target_nth={target_nth!} ratio={ratio!} onClick={() => {router.push({pathname:`/${type}/${e.id}`})}}>
+          {category.map((elem, idx) => (
+            <div>{e[elem[1]]}</div>
+          ))}
         </S.ContentWrapper>
       ))}
     </S.ContentContainer>
