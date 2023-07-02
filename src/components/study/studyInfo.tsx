1066ed70335d7e3e62bc835cec05f6558047f75e
jeongdopark <jdpark0816@gmail.com>
2023-07-02 14:27:34 +0900

    feat: 스터디 정보 수정

--- src/components/study/studyInfo.tsx
+++ src/components/study/studyInfo.tsx
@@ -1,13 +1,24 @@
 import { IMG_URL } from "@/pages/mock";
 import { S } from "./style";
+import { studyApi } from "@/api/studyApi";
+import { useRouter } from "next/router";
 
 const StudyInfo = () => {
+  const router = useRouter();
+  const {detail: param} = router.query;
+  const {data, isLoading} = studyApi.useGetStudyInfoQuery(param)
+  console.log(data)
+  
+  if(isLoading) return <div>Loading...</div>
   return (
     <S.Container>
       <S.Img src={IMG_URL} />
       <S.StudyInfoContainer>
-        <div>스터디 이름</div>
-        <button>가입하기</button>
+        <div>{data.data.name}</div>
+        <p style={{color:'white'}}>{data.data.about}</p>
+        {/* <button>가입하기</button> */}
+        <button>규칙 만들기</button>
+        <button onClick={(e) =>{e.preventDefault(); router.push({pathname:"/study/create", query:{name:data.data.name, id:param, about:data.data.about, capacity: data.data.capacity }})}}>스터디 수정하기</button>
       </S.StudyInfoContainer>
     </S.Container>
   );
--- src/pages/study/create.tsx
+++ src/pages/study/create.tsx
@@ -3,7 +3,7 @@ import { S } from "./style";
 import Input from "@/components/common/Input";
 import Slider from "@/components/slider/slider";
 import useInput from "@/hooks/useInput";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const CreateStudy = () => {
@@ -11,20 +11,33 @@ const CreateStudy = () => {
   const [aboutValue, setAboutValue, aboutHandler] = useInput('')
   const [memberCount, setMemberCount] = useState<number>(1);
   const router = useRouter();
-  
-  const [createStudy, {isLoading}] = studyApi.useCreateStudyMutation();
+  const isEditMode = Object.keys(router.query).length !== 0 ? true : false;
+  const [createStudy] = studyApi.useCreateStudyMutation();
+  const [updateStudy] = studyApi.useUpdateStudyMutation();
 
-  const handleCreateStudy = (e:any) => {
+  useEffect(() => {
+    setNameValue(router.query.name)
+    setAboutValue(router.query.about)
+    setMemberCount(router.query.capacity)
+  }, [])
+  
+
+  const handleSubmit = (e:any) => {
     e.preventDefault(); 
-    createStudy({"member":1, "name":nameValue, "about":aboutValue, "leader":"leader","capacity":memberCount})
-    router.push({pathname:"/profile"})
-}
+    if(isEditMode){
+      updateStudy({"id": router.query.id, "name":nameValue, "about":aboutValue,"capacity":memberCount})
+      router.push({pathname:`${router.query.id}`})
+    }else{
+      createStudy({"member":1, "name":nameValue, "about":aboutValue, "leader":"leader","capacity":memberCount})
+      router.push({pathname:"/profile"})
+    }
+  }
   return (
-    <S.Container onSubmit={(e) => handleCreateStudy(e)}>
+    <S.Container onSubmit={(e) => handleSubmit(e)}>
       <Input title={"스터디 이름"} size={"40%"} value={nameValue} onChange={nameHandler}/>
       <Input title={"스터디 소개"} size={"40%"} value={aboutValue} onChange={aboutHandler}/>
       <Slider memberCount={memberCount} setMemberCount={setMemberCount}/>
-      <S.Button type="submit" value={'스터디 생성'}/>
+      {isEditMode ? <S.Button type="submit" value={'스터디 수정'}/> : <S.Button type="submit" value={'스터디 생성'}/>}
     </S.Container>
   );
 };
