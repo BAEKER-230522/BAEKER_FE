69fc9907ce00649a33cfbbf1628cf0206a5d5892
jeongdopark <jdpark0816@gmail.com>
2023-07-02 11:58:56 +0900

    feat: 규칙 등록 버튼, router 적용

--- src/components/rule/search.tsx
+++ src/components/rule/search.tsx
@@ -1,9 +1,12 @@
+import { useRouter } from "next/router";
 import { S } from "./style";
 
 const Search = () => {
+  const router = useRouter()
+  
   return (
     <S.Container>
-      <S.CreateButton>규칙 등록</S.CreateButton>
+      <S.CreateButton onClick={() => router.push({pathname:"/rule/create"})}>규칙 등록</S.CreateButton>
       <S.InputContainer>
         <S.Input placeholder="검색어를 입력해 주세요" />
         <S.Button>
