a8f4763d5d40b5a508d97e3f1996dfe4cbc08eee
jeongdopark <jdpark0816@gmail.com>
2023-07-02 11:59:43 +0900

    feat: 규칙 리스트

--- src/pages/rule/list.tsx
+++ src/pages/rule/list.tsx
@@ -1,12 +1,16 @@
 import Board from "@/components/common/board/Board";
 import Search from "@/components/rule/search";
 import { S } from "./style";
+import { ruleApi } from "@/api/ruleApi";
 
 const Rule = () => {
+  const {data, isLoading} = ruleApi.useGetAllRulesQuery({});
+  
+  if(isLoading) return <div>Loading...</div>
   return (
     <S.Container>
       <Search />
-      <Board category={["번호", "제목", "OJ 사이트", "작성일"]} widthRatio={[1, 1, 1, 2]} />;
+      <Board category={[["제목", "name"], ["소개", "about"], ["OB","provider" ]]} widthRatio={[1, 2, 1]} data={data.data} type={"rule"}/>;
     </S.Container>
   );
 };
