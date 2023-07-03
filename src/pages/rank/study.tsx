3a1420ae81199ab3fedb7c0e1c6a668fe45710d5
jeongdopark <jdpark0816@gmail.com>
2023-07-03 11:32:32 +0900

    feat: 스터디, 유저 리스트

--- src/pages/rank/study.tsx
+++ src/pages/rank/study.tsx
@@ -1,10 +1,16 @@
 import { S } from "./style";
 import Board from "@/components/common/board/Board";
+import { studyApi } from "@/api/studyApi";
 
 const StudyRank = () => {
+  const {data, isLoading} = studyApi.useGetStudyInfoListQuery(0)
+  console.log(data);
+
+  if(isLoading) return <div>Loading...</div>
+  
   return (
     <S.Container>
-      <Board category={["랭킹", "스터디", "소개", "인원", "방장"]} widthRatio={[1, 1, 2, 1, 1]} />;
+      <Board category={[ ["스터디", "name"], ["소개", "about"], ["인원", "capacity"], ["방장", "leader"]]} widthRatio={[1, 2, 1, 1]} data={data.data} type={"study"}/>
     </S.Container>
   );
 };
