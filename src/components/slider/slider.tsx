8797672fffd671b770ee5cc2b4b316f36c510252
jeongdopark <jdpark0816@gmail.com>
2023-07-14 00:29:47 +0900

    chore: 변수명 변경

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { S } from "./style";
 
 interface IProps {
-  memberCount: number;
-  setMemberCount: React.Dispatch<React.SetStateAction<number>>
+  maxStudyCapacity: number;
+  setMaxStudyCapacity: React.Dispatch<React.SetStateAction<number>>
 }
 const Slider = ({...props}:IProps) => {
-  console.log(props);
+
   const test = (e: any) => {
-    console.log('test');
-    props.setMemberCount(e.target.value);
+    props.setMaxStudyCapacity(e.target.value);
   };
 
   const arr = [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 
   return (
     <S.Container>
-      <S.Title>스터디 최대 인원({props.memberCount}/20)</S.Title>
-      <S.Slider type="range" onChange={test} min={1} max={20} value={props.memberCount} />
+      <S.Title>스터디 최대 인원({props.maxStudyCapacity}/20)</S.Title>
+      <S.Slider type="range" onChange={test} min={1} max={20} value={props.maxStudyCapacity} />
       <S.Value>{arr.map((e, idx) => (e === 1 ? <span key={idx}>{idx + 1}</span> : <span key={idx}></span>))}</S.Value>
     </S.Container>
   );
