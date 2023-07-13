1cd30c17c10b7335774f595f15ddb2a2abe494d5
jeongdopark <jdpark0816@gmail.com>
2023-07-13 21:34:48 +0900

    chore: 불필요한 log 삭제

--- src/pages/study/[detail].tsx
+++ src/pages/study/[detail].tsx
@@ -12,10 +12,10 @@ import { useRouter } from "next/router";
 const StudyDetail = () => {
   const router = useRouter();
   const {detail: param} = router.query;  
-  const {data:studyMissionList, isLoading:getStudyMissionListLoading} = studyApi.useGetStudyRuleListQuery(param);
-  const {data:stduyMemberList, isLoading:getMemberListLoading} = studyApi.useGetStudyMemberListQuery(param);
-  const {data:studyPedingList, isLoading:getPedingListLoading} = studyApi.useGetPendingListQuery(param);
-  const {data:studyInfo, isLoading:getStudyInfoLoading} = studyApi.useGetStudyInfoQuery(param);
+  const {data:studyMissionList, isLoading:getStudyMissionListLoading} = studyApi.useGetStudyRuleListQuery(Number(param));
+  const {data:stduyMemberList, isLoading:getMemberListLoading} = studyApi.useGetStudyMemberListQuery(Number(param));
+  const {data:studyPedingList, isLoading:getPedingListLoading} = studyApi.useGetPendingListQuery(Number(param));
+  const {data:studyInfo, isLoading:getStudyInfoLoading} = studyApi.useGetStudyInfoQuery(Number(param));
   
   
   const tabState = useSelector((state: any) => {
@@ -24,33 +24,11 @@ const StudyDetail = () => {
   const TAB_ELEMENTS = ["현황", "미션", "멤버", "가입요청"];
 
   if(getMemberListLoading || getPedingListLoading ||getStudyMissionListLoading || getStudyInfoLoading) return <div>Loading ... </div>
-  console.log(studyMissionList);
-  // switch문으로 할 경우 pagination을 공유하게 된다. 왜그럴까 ?
-  // const Component = (num: number) => {
-  //   switch (num) {
-  //     case 0:
-  //       return (
-  //         <S.StatusContainer>
-  //           <SolveStatus />
-  //           <S.ChartContainer>
-  //             <SolvedRecord />
-  //             <LineChart />
-  //           </S.ChartContainer>
-  //         </S.StatusContainer>
-  //       );
-  //     case 1:
-  //       return <Board category={["규칙", "소개", "작성일"]} widthRatio={[1, 2, 1]} test={1} />;
-  //     case 2:
-  //       return <Board category={["이름", "랭킹", "가입한 스터디"]} widthRatio={[2, 1, 1]} test={2} />;
-  //     case 3:
-  //       return <Board category={["이름", "랭킹", "상태"]} widthRatio={[1, 1, 1]} test={3} />;
-  //   }
-  // };
+  
   return ( 
     <S.StudyContainer>
       <StudyInfo />
       <Tab elements={TAB_ELEMENTS} type="study" />
-      {/* <S.ContentContainer>{Component(tabState)}</S.ContentContainer> */}
       <S.ContentContainer>
         {tabState === 0 && (
           <>
@@ -63,7 +41,6 @@ const StudyDetail = () => {
             </S.StatusContainer>
           </>
         )} 
-        
         {tabState === 1 && <Board type={"study/mission"} category={[["규칙", "name"], ["소개", "about"], ["작성일", "createDate"]]} widthRatio={[1, 2, 1]}  data={studyMissionList.data}/>}
         {tabState === 2 && <Board type={"member"} category={[["이름", "nickname"], ["랭킹", "ruby"], ["가입한 스터디", "id"]]} widthRatio={[2, 1, 1]} data={stduyMemberList.data}/>}
         {tabState === 3 && <Board type={"join"} category={[["이름", "nickname"], ["랭킹", "ruby"], ["상태", "invite"]]} widthRatio={[1, 1, 1]} data={studyPedingList.data.pending} />}
