31449e2eed94cfaa9d95dcaeec8c6b4a42be3e96
jeongdopark <jdpark0816@gmail.com>
2023-07-15 00:17:02 +0900

    feat: kakao redirect

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,17 @@
+import axios from "axios";
 import { S } from "./style";
+import Link from "next/link";
 
 const Home = () => {
+  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+  const KAKAO_REDIRECT_URI = "http://localhost:3000/login/kakao";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+
+  const onClick = async() => {
+    const res = await axios({method:'post', url:"http://101.101.208.240:9000/oauth2/authorization/kakao"})
+    console.log(res)
+  }
+
   return (
     <S.Container>
       <S.Wrapper>
@@ -11,7 +22,7 @@ const Home = () => {
           <br />
           다양한 규칙 수행 보상을 통해 스터디 랭커에 도전해 보세요!
         </div>
-        <S.KakaoButton />
+        <S.KakaoButton onClick={onClick}/>
       </S.Wrapper>
     </S.Container>
   );
