2f9a7272d5f684f0493ff05c46e4d9a48d1c006b
jeongdopark <jdpark0816@gmail.com>
2023-07-15 17:52:07 +0900

    feat: 미션 문제 삭제 버튼

---
+++ src/components/common/board/RemoveProbleButton.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { S } from './style';
+import { Button } from 'antd';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+import { useDispatch } from 'react-redux';
+import * as userAction from "@/store/modules/missionProblem";
+
+const RemoveProblemButton = ({idx}:{idx:number}) => {
+  const dispatch = useDispatch();
+  const [size, setSize] = useState<SizeType>('middle'); // default is 'middle'
+
+  const handleRemoveMissionProblem = (e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation()  
+    dispatch(userAction.removeProblem(idx))
+  }
+
+  return (
+      <S.ButtonWrapper>
+        <Button type="primary" size={size} onClick={(e) => handleRemoveMissionProblem(e)}>
+          삭제
+        </Button>
+      </S.ButtonWrapper>
+)};
+
+export default RemoveProblemButton;
