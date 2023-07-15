83b2c79ce5b2419a36ff08c45ecfa782a2709d2c
jeongdopark <jdpark0816@gmail.com>
2023-07-15 17:46:19 +0900

    feat: RangeDatePicker 컴포넌트

---
+++ src/components/Calendar/RangeDatePicker.tsx
@@ -0,0 +1,36 @@
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale'; //한국어 설정
+import React, { useState } from 'react';
+import { S } from './style';
+
+const StartToEndRangeDatePicker = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  console.log(startDate.toLocaleDateString(), endDate.toLocaleDateString());
+  
+  return (
+    <S.Container>
+      <DatePicker
+        selected={startDate}
+        onChange={(date:Date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        locale={ko}
+      />
+      <div style={{color:'white'}}>ㅡ</div>
+      <DatePicker
+        selected={endDate}
+        onChange={(date:Date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+        locale={ko}
+      />
+    </S.Container>
+  );
+};
+
+export default StartToEndRangeDatePicker;
