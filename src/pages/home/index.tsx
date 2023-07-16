748d6e24c3e06e4fbd80c2d8bb0f548e511dec79
jeongdopark <jdpark0816@gmail.com>
2023-07-16 21:09:44 +0900

    feat: kakao

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,13 +3,15 @@ import { S } from "./style";
 import Link from "next/link";
 
 const Home = () => {
-  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+
+  // const KAKAO_REST_API_KEY = 502e1203fe869885c48c863f2eda58bf;
   const KAKAO_REDIRECT_URI = "http://localhost:3000/login/kakao";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=502e1203fe869885c48c863f2eda58bf&redirect_uri=http://localhost:3000/login/kakao&response_type=code`;
 
   const onClick = async() => {
-    const res = await axios({method:'post', url:"http://101.101.208.240:9000/oauth2/authorization/kakao"})
-    console.log(res)
+    // const res = await axios({method:'get', url:"http://101.101.208.240:9000/oauth2/authorization/kakao"})
+    window.location.href = KAKAO_AUTH_URL
+    // console.log(res)
   }
 
   return (
