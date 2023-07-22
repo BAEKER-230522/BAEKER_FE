929ac3717c2d23f83ef14ec735a2329442a68081
jeongdopark <jeongdo.dev@gmail.com>
2023-07-22 17:34:08 +0900

    feat: 날짜 형식 0000-00-00

--- src/components/Calendar/RangeDatePicker.tsx
+++ src/components/Calendar/RangeDatePicker.tsx
@@ -1,13 +1,39 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { ko } from 'date-fns/esm/locale'; //한국어 설정
-import React, { useState } from 'react';
+import React, { useState, Dispatch, useEffect } from 'react';
 import { S } from './style';
 
-const StartToEndRangeDatePicker = () => {
+interface IProps{
+  setMissionStartDate : React.Dispatch<React.SetStateAction<string>>,
+  setMissionEndDate : React.Dispatch<React.SetStateAction<string>>
+}
+const StartToEndRangeDatePicker = ({ setMissionStartDate, setMissionEndDate } : IProps ) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  console.log(startDate.toLocaleDateString(), endDate.toLocaleDateString());
+  console.log(startDate, endDate.toLocaleDateString());
+
+  useEffect(() => {
+    let start = startDate.toLocaleDateString().slice(0, -1).split('. ')
+    
+    for(let i=0; i<start.length; i++){
+      if(start[i].length === 1){
+        start[i] = '0'+start[i]
+      }
+    }
+    setMissionStartDate(start.join('-'))
+    
+  }, [startDate])
+
+  useEffect(() => {
+    let end = endDate.toLocaleDateString().slice(0, -1).split('. ')
+    for(let i=0; i<end.length; i++){
+      if(end[i].length === 1){
+        end[i] = '0'+end[i]
+      }
+    }
+    setMissionEndDate(end.join('-'))
+  }, [endDate])
   
   return (
     <S.Container>
