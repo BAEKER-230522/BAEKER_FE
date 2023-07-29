64e3e06e199715543bc5e368eacdc34c8286fe85
jeongdopark <jeongdo.dev@gmail.com>
2023-07-29 19:58:55 +0900

    Feat: userId 전역상태 관리 & 서버사이드에서 userId 쿠키에서 가져오기

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next'
+import { parseCookies } from '@/util/parseCookie';
 import { S } from "./style";
 import LineChart from "@/components/Chart/Chart";
 import UserInfo from "@/components/UserInfo/UserInfo";
@@ -5,19 +7,53 @@ import UserSolvedInfo from "@/components/UserInfo/UserSolvedInfo";
 import Tab from "@/components/Tab/Tab";
 import SolvedRecord from "@/components/Tab/SolvedRecord";
 import Board from "@/components/common/Board/Board";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { USER_NUMBER } from "@/util/constant";
 import useFetchUserStudyList from "@/hooks/queries/useFetchUserStudyList";
 import useFetchUserData from "@/hooks/queries/useFetchUserData";
 import useTabSwitch from "@/hooks/useTabSwitch";
 import Loading from "@/components/Loading/Loading";
+import { loginUser } from '@/store/modules/user';
+import { useEffect, useState } from 'react';
 
-const Profile = () => {
+interface LoginProps {
+  refreshToken: string;
+  memberId: number;
+}
+
+export const getServerSideProps : GetServerSideProps = async(context) => {
+  const {req, res} = context;
+  const cookies = parseCookies(req.headers.cookie)
+  const refreshToken = cookies.refreshToken
+  const memberId = Number(cookies.memberId)
+
+  console.log('refresh:', refreshToken);
+  console.log('memberId', memberId);
+  
+  
+  return {
+    props: {
+      refreshToken,
+      memberId,
+    },
+  };
+}
+
+const Profile = ({ refreshToken, memberId }: LoginProps) => {
+  const dispatch = useDispatch()
+  console.log('memberId:', memberId);
+  
+  useEffect(() => {
+    localStorage.setItem('refreshToken', refreshToken)
+    dispatch(loginUser(memberId))
+  }, [])
+  
   const TAB_ELEMENTS = ["백준", "스터디", "가입 신청", "가입 초대"];
-  const {data: userStudyList, isLoading: isStudyListLoading} = useFetchUserStudyList({memberId:USER_NUMBER,status:1});
-  const {data: userStudyJoinRequestList, isLoading: isStudyJoinRequestListLoading} = useFetchUserStudyList({memberId:USER_NUMBER,status:2});
-  const {data: userStudyInviteList, isLoading: isStudyInviteListLoading} = useFetchUserStudyList({memberId:USER_NUMBER,status:3});
-  const {data: userData, isLoading:isUserDataLoading} = useFetchUserData(USER_NUMBER);
+  console.log(useSelector((state) => state.user.userId));
+  const {data: userStudyList, isLoading: isStudyListLoading} = useFetchUserStudyList({memberId,status:1});
+  const {data: userStudyJoinRequestList, isLoading: isStudyJoinRequestListLoading} = useFetchUserStudyList({memberId,status:2});
+  const {data: userStudyInviteList, isLoading: isStudyInviteListLoading} = useFetchUserStudyList({memberId,status:3});
+  const {data: userData, isLoading:isUserDataLoading} = useFetchUserData(memberId);
   
   const tabState = useSelector((state: any) => {
     return state.tab.profileTabState;
