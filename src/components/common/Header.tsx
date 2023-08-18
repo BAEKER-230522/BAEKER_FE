703306cbaa815fadc5ec28e4a7c7175c9a2f6256
Xeraph <jeongdo.dev@gmail.com>
2023-08-18 20:46:33 +0900

    Fix: deploy issue 대소문자 구분을 vercel에서는 한다.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import { S } from "./style";
-import DropDown from "../Dropdown/index";
+import DropDown from "../DropDown/index";
 import { useOncClickIcon } from "@/hooks/useOnClickIcon";
 import Link from "next/link";
 import { useSelector } from "react-redux";
