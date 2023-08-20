987a9cb859158205a20a04e9e5c6d50fe9c3bdef
Xeraph <jeongdo.dev@gmail.com>
2023-08-20 16:02:36 +0900

    Chore: middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const isLogin = request.cookies.get("accessToken")
+  console.log(isLogin, '1');
   const restrictedPages = ["/home", "/profile", "/study/manage", "/rule"];
   const currentPath = request.nextUrl.pathname;
 
   if(isLogin === undefined && restrictedPages.includes(currentPath)){
+    console.log(isLogin, '2');
     return NextResponse.redirect(`${request.nextUrl.origin}`);
   }
   if(isLogin !== undefined){
     if(currentPath === '/') {
+      console.log(isLogin, '3');
       return NextResponse.redirect(`${request.nextUrl.origin}/home`);
     }
   }
