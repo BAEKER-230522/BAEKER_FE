70eaed52c1d70af17fa61f24af1bea3e20e35ebd
Xeraph <jeongdo.dev@gmail.com>
2023-08-22 22:32:06 +0900

    Feat: 유저 상세페이지, 분기처리

--- src/components/userInfo/UserInfo.tsx
+++ src/components/userInfo/UserInfo.tsx
@@ -2,6 +2,7 @@ import { IMG_URL } from "../../../public/mock";
 import { S } from "./style";
 import { useRouter } from "next/router";
 import JoinRequestModal from "../common/Modal/JoinRequestModal";
+import { useSelector } from "react-redux";
 
 interface IProps {
   userData : any;
@@ -10,14 +11,13 @@ interface IProps {
 
 const UserInfo = ({userData, userId}:IProps) => {
   const router = useRouter()
-  const isMypage = Object.keys(router.query).length === 0 ? true : false;
-
+  const id = useSelector((state:any) => {return state.user.memberId})
   return (
     <S.Container>
-      <S.Image src={IMG_URL} />
+      <S.Image src={IMG_URL}/>
       <S.Name>{userData.nickname}</S.Name>
       <S.Introduce>{userData.about}</S.Introduce>
-      {isMypage? <S.Button onClick={() => router.push({pathname:"/modify"})}>프로필 수정</S.Button> : userId === null ? null :  <JoinRequestModal name={userData.nickname} id={userData.id} userId={userId}/>}
+      {id === userId ? <S.Button onClick={() => router.push({pathname:"/modify"})}>프로필 수정</S.Button> : userId === null ? null :  <JoinRequestModal name={userData.nickname} id={userData.id} userId={userId}/>}
     </S.Container>
   );
 };
