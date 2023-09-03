f8f022476f20a9a6d03451919158e434fb46f4ec
Xeraph <jeongdo.dev@gmail.com>
2023-09-03 19:33:19 +0900

    Update: 카카오톡 프로필 사진으로 변경

--- src/components/common/user-info/user-info.tsx
+++ src/components/common/user-info/user-info.tsx
@@ -14,7 +14,7 @@ const UserInfo = ({userData, userId}:IProps) => {
   const id = useSelector((state:any) => {return state.user.memberId})
   return (
     <S.Container>
-      <S.Image src={IMG_URL}/>
+      <S.Image src={userData.profileImg}/>
       <S.Name>{userData.nickname}</S.Name>
       <S.Introduce>{userData.about}</S.Introduce>
       {id === userId ? <S.Button onClick={() => router.push({pathname:"/modify"})}>프로필 수정</S.Button> : userId === null ? null :  <JoinRequestModal name={userData.nickname} id={userData.id} userId={userId}/>}
