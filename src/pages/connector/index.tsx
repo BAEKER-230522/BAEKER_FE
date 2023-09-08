aeb482c2913862ef56bff5479939c38f9024e98d
Xeraph <jeongdo.dev@gmail.com>
2023-09-08 14:06:01 +0900

    Fix: connector 페이지

--- src/pages/connector/index.tsx
+++ src/pages/connector/index.tsx
@@ -1,18 +1,18 @@
-import ModifyImg from "@/components/modify/Img";
+import styled from "styled-components";
 import { themedPalette } from "@/styles/theme";
+import ModifyImg from "@/components/modify/Img";
 import Input from "@/components/common/input";
 import ModifyButton from "@/components/modify/button";
 import { memberApi } from "@/api/memberApi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useInput from "@/hooks/useInput";
 import useUpdateUserInfo from "@/hooks/useUpdateUserInfo";
 import Loading from "@/components/common/loading/Loading";
 import { parseCookies } from "@/util/parseCookie";
 import { GetServerSideProps } from "next";
-import LocalStorage from "@/util/localstorage";
-import styled from "styled-components";
 import { PageContainer } from "@/styles/common.style";
+
 interface LoginProps {
   refreshToken: string;
   memberId: number;
@@ -24,25 +24,26 @@ interface IParsedCookies {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { req, res } = context;
+  const { req } = context;
   const cookies: IParsedCookies = parseCookies(req.headers.cookie);
-  const refreshToken = cookies.refreshToken;
   const memberId = Number(cookies.memberId);
+  const refreshToken = cookies.refreshToken;
 
   return {
     props: {
-      refreshToken,
       memberId,
+      refreshToken,
     },
   };
 };
 
-const Connector = ({ memberId, refreshToken }: LoginProps) => {
+const Connector = ({ memberId }: LoginProps) => {
   const { data, isLoading } = memberApi.useGetMemberQuery(memberId);
   const [nameValue, setNameValue, onChangeName] = useInput("");
   const [aboutValue, setAboutValue, onChangeAbout] = useInput("");
-  const [baekjoonIdValue, setBaekjoonIdValue, onChangebaekjoonId] =
-    useInput("");
+  const [baekjoonIdValue, setBaekjoonIdValue, onChangebaekjoonId] = useInput("");
+  const [img, setImg] = useState("");
+  const [imgFile, setImgFile] = useState<File | undefined>(undefined);
   const { handleUpdateUserInfo } = useUpdateUserInfo(memberId);
   const [connectBaekJoon] = memberApi.useConnectBaekjoonMutation();
   const router = useRouter();
@@ -50,19 +51,14 @@ const Connector = ({ memberId, refreshToken }: LoginProps) => {
     if (isLoading === false) {
       setNameValue(data.data.nickname);
       setAboutValue(data.data.about);
+      setImg(data.data.kakaoProfileImage);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading]);
 
-  useEffect(() => {
-    LocalStorage.setItem("refreshToken", refreshToken);
-    LocalStorage.setItem("memberId", String(memberId));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const onSubmitUpdateUserInfo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    handleUpdateUserInfo({ nameValue, aboutValue });
+    handleUpdateUserInfo({ nameValue, aboutValue, imgFile });
     connectBaekJoon({ memberId, baekjoonId: baekjoonIdValue });
     router.push({ pathname: "/profile" });
   };
@@ -80,25 +76,10 @@ const Connector = ({ memberId, refreshToken }: LoginProps) => {
   return (
     <S.Container>
       <S.FormContainer onSubmit={(e) => onSubmitUpdateUserInfo(e)}>
-        <ModifyImg userImg={data.data.kakaoProfileImage} />
-        <Input
-          title={"이름"}
-          size={"25%"}
-          value={nameValue}
-          onChange={onChangeName}
-        />
-        <Input
-          title={"자기소개"}
-          size={"25%"}
-          value={aboutValue}
-          onChange={onChangeAbout}
-        />
-        <Input
-          title={"백준 연동 ID"}
-          size={"25%"}
-          value={baekjoonIdValue}
-          onChange={onChangebaekjoonId}
-        />
+        <ModifyImg img={img} setImg={setImg} setImgFile={setImgFile} />
+        <Input title={"이름"} size={"25%"} value={nameValue} onChange={onChangeName} />
+        <Input title={"자기소개"} size={"25%"} value={aboutValue} onChange={onChangeAbout} />
+        <Input title={"백준 연동 ID"} size={"25%"} value={baekjoonIdValue} onChange={onChangebaekjoonId} />
         <ModifyButton />
       </S.FormContainer>
     </S.Container>
