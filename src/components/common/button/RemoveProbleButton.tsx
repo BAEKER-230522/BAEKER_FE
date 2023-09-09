7bc76014dbec6c4db4225d47caa14c205484f315
Xeraph <jeongdo.dev@gmail.com>
2023-09-09 23:26:12 +0900

    Rename: 파일 경로 이동

---
+++ src/components/common/button/RemoveProbleButton.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { S } from "../table/style";
+import { Button } from "antd";
+import type { SizeType } from "antd/es/config-provider/SizeContext";
+import { useDispatch } from "react-redux";
+import * as userAction from "@/store/modules/mission";
+
+const RemoveProblemButton = ({ idx }: { idx: number }) => {
+  const dispatch = useDispatch();
+  const [size, setSize] = useState<SizeType>("middle"); // default is 'middle'
+
+  const handleRemoveMissionProblem = (e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    dispatch(userAction.removeProblem(idx));
+  };
+
+  return (
+    <S.ButtonWrapper>
+      <Button type="primary" size={size} onClick={(e) => handleRemoveMissionProblem(e)}>
+        삭제
+      </Button>
+    </S.ButtonWrapper>
+  );
+};
+
+export default RemoveProblemButton;
