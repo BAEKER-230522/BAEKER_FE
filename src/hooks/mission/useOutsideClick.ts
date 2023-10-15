ac6ea1635bb1327da7dbce8ddf72f6e111794df2
Xeraph <jeongdo.dev@gmail.com>
2023-10-15 10:39:12 +0900

    feat: 모달 외부영역 클릭 시 닫히는 customhook

--- src/hooks/mission/useOutsideClick.ts
+++ src/hooks/mission/useOutsideClick.ts
@@ -2,23 +2,29 @@ import React, { useEffect } from "react";
 
 interface IProps {
   ref: React.RefObject<HTMLDivElement>;
-  isFocus: boolean;
-  setFocus: React.Dispatch<React.SetStateAction<boolean>>;
+  isInit: boolean;
+  setIsInit: React.Dispatch<React.SetStateAction<boolean>>;
   isOpened: boolean;
   setIsOpened: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const useOutsideClick = ({ ref, isFocus, setFocus, isOpened, setIsOpened }: IProps) => {
-  console.log(isFocus, 1);
-
+const useOutsideClick = ({ ref, isInit, setIsInit, isOpened, setIsOpened }: IProps) => {
   const handleFocusOut = (event: MouseEvent) => {
-    console.log(ref.current?.contains(event.target as Node), isFocus, 2);
-    if (!ref.current?.contains(event.target as Node) && isOpened) {
-      setFocus(false);
-      setIsOpened(false);
+    console.log(ref.current?.contains(event.target as Node));
+    // 최초 모달 오픈
+    if (isInit) {
+      setIsInit(false);
+      return;
     }
+    // 모달 밖 클릭 & 모달 열려 있을 경우
+    if (!ref.current?.contains(event.target as Node) && isOpened) {
+      setIsOpened(false);
+      setIsInit(false);
+    }
+    // 모달 밖 클릭 & 모달 열려 있지 않은 경우
     if (!ref.current?.contains(event.target as Node) && !isOpened) {
-      setIsOpened(true);
+      setIsOpened(false);
+      setIsInit(false);
     }
   };
   useEffect(() => {
