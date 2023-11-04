f238926e3dd431466430327b967ad2764e0e802e
Xeraph <jeongdo.dev@gmail.com>
2023-11-05 03:56:52 +0900

    refactor

--- src/pages/connector/index.tsx
+++ src/pages/connector/index.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { themedPalette } from "@/styles/theme";
-import ModifyImg from "@/components/modify/Img";
+import ModifyImg from "@/components/modify/img";
 import Input from "@/components/common/input";
-import ModifyButton from "@/components/modify/Button";
+import ModifyButton from "@/components/modify/button";
 import { memberApi } from "@/api/memberApi";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
